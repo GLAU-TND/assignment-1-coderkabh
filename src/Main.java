4d174c06136067a647d6f1ed3f380d9529ff2d48
kanishk <kanishkbharadwaj333@gmail.com>
2020-02-23 22:31:11 +0530

    add main class

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+//    Welcome to DBC's Contact List App
+//    Press 1 to add a new contact
+//    Press 2 to view all contacts
+//    Press 3 to search for a contact
+//    Press 4 to delete a contact
+//    Press 5 to exit program
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int choice = scan.nextInt();
+        while (true) {
+            System.out.println("Welcome to coderkabh's Contact List App");
+            System.out.println("Press 1 to add a new contact");
+            System.out.println("Press 2 to view all contacts");
+            System.out.println("Press 3 to search for a contact");
+            System.out.println("Press 4 to delete a contact");
+            System.out.println("Press 5 to exit program");
+
+            if (choice == 1) {
+
+            }
+        }
+    }
+
+}
